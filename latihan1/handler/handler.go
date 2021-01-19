b68f1ca0514d0222660ec7a549665986c5898287
Danil Syah Arihardjo <danilsyaharihardjo@gmail.com>
2021-01-19 13:57:40 +0700

    form

--- latihan1/handler/handler.go
+++ latihan1/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"html/template"
+	"latihan1/entity"
 	"log"
 	"net/http"
 	"path"
@@ -25,10 +26,20 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// data map , tipe key = string , tipe data value = bebas
-	data := map[string]interface{}{
-		"title" 	: "I'm learning Golang Web",
-		"id" 			: 1234,
-		"content" : "I'm learning Golang WEB with BWA",
+	// data := map[string]interface{}{
+	// 	"title" 	: "I'm learning Golang Web",
+	// 	"id" 			: 1234,
+	// 	"content" : "I'm learning Golang WEB with BWA",
+	// }
+
+	// passing struct to view
+	// data := entity.Product{ID: 112345, Name: "Honda Jazz", Price: 250000000, Stock: 4}
+
+	// slice of struct
+	data := []entity.Product{
+		{ID: 1001, Name: "Mobilio",Price: 220000000, Stock: 8},
+		{ID: 1002, Name: "Xpander",Price: 270000000, Stock: 2},
+		{ID: 1003, Name: "Mobilio",Price: 500000000, Stock: 13},
 	}
 
 	// err = tmpl.Execute(w, nil) // tidak mengirim data ke html
@@ -77,4 +88,77 @@ func ProductHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
 		return
 	}
+}
+
+func PostGet(w http.ResponseWriter, r *http.Request){
+	method := r.Method
+
+	switch method{
+	case "GET":
+		w.Write([]byte("Ini adalah GET"))
+	case "POST":
+		w.Write([]byte("ini adalah POST"))
+	default:
+		http.Error(w, "Error is happening, keep calm", http.StatusBadRequest)
+	}
+}
+
+func Form(w http.ResponseWriter, r *http.Request){
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles(path.Join("views", "form.html"), path.Join("views/templates","layout.html"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error is Happening, keep calm", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	http.Error(w, "Error is happening, keep calmmm", http.StatusBadRequest)
+}
+
+func Process(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST"{
+		err := r.ParseForm()
+		if err != nil{
+			log.Println(err)
+			http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+			return
+		}
+
+		name := r.Form.Get("name")
+		message := r.Form.Get("message")
+
+		data := map[string]interface{}{
+			"name": name,
+			"message": message,
+		}
+
+		tmpl, err := template.ParseFiles(path.Join("views","result.html"), path.Join("views/templates", "layout.html"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+			return
+		}
+
+		// w.Write([]byte(name))
+		// w.Write( []byte(message))
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+			return
+		}
+
+		return
+	}
+
+	http.Error(w, "Error is happening, keep calm", http.StatusBadRequest)
 }
--- latihan1/main.go
+++ latihan1/main.go
@@ -14,20 +14,25 @@ func main() {
 	aboutHandler := func (w http.ResponseWriter, r *http.Request)  {
 		w.Write([]byte("About Page"))
 	}
-
+	// anonymous function
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("halaman test"))
+	})
 	mux.HandleFunc("/", handler.HomeHandler) // routing root
 	mux.HandleFunc("/hello", handler.HelloHandler)
 	mux.HandleFunc("/profile", handler.ProfileHandler)
 	mux.HandleFunc("/product", handler.ProductHandler)
 	mux.HandleFunc("/about",aboutHandler)
-	// anonymous function
-	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("halaman test"))
-	})
+	mux.HandleFunc("/post-get", handler.PostGet)
+	mux.HandleFunc("/form", handler.Form)
+	mux.HandleFunc("/process", handler.Process)
 
+	// static file css,js,image
+	fileServer := http.FileServer(http.Dir("assets"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting web on port 8081")
+	log.Println("Starting web on port 8080")
 
-	err := http.ListenAndServe(":8081", mux)
+	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
